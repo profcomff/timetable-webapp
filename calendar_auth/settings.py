695e8aedf40008204ecf5a60513f2e2480f0d65e
Mikhail <87110754+mixx3@users.noreply.github.com>
2022-04-20 17:25:50 +0300

    Update settings.py
    add settings for google flow

--- calendar_auth/settings.py
+++ calendar_auth/settings.py
@@ -6,13 +6,16 @@ class Settings(BaseSettings):
     """Application settings"""
 
     DB_DSN: PostgresDsn
-    GOOGLE_CREDS: Json
+    PATH_TO_GOOGLE_CREDS: str
     APP_URL: Optional[AnyHttpUrl] = None
     REDIRECT_URL: AnyHttpUrl = "https://www.profcomff.com"
     GROUPS: List[str] = ["101", "102"]
+    DEFAULT_GROUP = '{group: 0}'
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
 
     class Config:
         """Pydantic BaseSettings config"""
 
         case_sensitive = True
         env_file = ".env"
+
