1e07896ffba0dd26c2a8106715054efe0f9d40aa
Mikhail <87110754+mixx3@users.noreply.github.com>
2022-04-22 20:15:27 +0300

    Update settings.py
    additional settings for google oauth flow on backend

--- calendar_auth/settings.py
+++ calendar_auth/settings.py
@@ -16,3 +16,24 @@ class Settings(BaseSettings):
 
         case_sensitive = True
         env_file = ".env"
+
+from pydantic import BaseSettings, Json, PostgresDsn, AnyHttpUrl
+from typing import List, Optional
+
+
+class Settings(BaseSettings):
+    """Application settings"""
+
+    DB_DSN: PostgresDsn
+    PATH_TO_GOOGLE_CREDS: str
+    APP_URL: Optional[AnyHttpUrl] = None
+    REDIRECT_URL: AnyHttpUrl = "https://www.profcomff.com"
+    GROUPS: List[str] = ["101", "102"]
+    DEFAULT_GROUP = '{group: 0}'
+    SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/userinfo.email']
+
+    class Config:
+        """Pydantic BaseSettings config"""
+
+        case_sensitive = True
+        env_file = ".env"
