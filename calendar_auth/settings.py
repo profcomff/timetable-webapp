b84cb8b8ca4eeb16c002d857d7f9a1a084f20baa
Mikhail <87110754+mixx3@users.noreply.github.com>
2022-04-23 21:23:19 +0300

    Update settings.py

--- calendar_auth/settings.py
+++ calendar_auth/settings.py
@@ -6,10 +6,12 @@ class Settings(BaseSettings):
     """Application settings"""
 
     DB_DSN: PostgresDsn
-    GOOGLE_CREDS: Json
+    PATH_TO_GOOGLE_CREDS: str
     APP_URL: Optional[AnyHttpUrl] = None
     REDIRECT_URL: AnyHttpUrl = "https://www.profcomff.com"
     GROUPS: List[str] = ["101", "102"]
+    DEFAULT_GROUP = '{group: 0}'
+    SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/userinfo.email']
 
     class Config:
         """Pydantic BaseSettings config"""
